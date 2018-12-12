90b53ae7d50880192e9b97454a89146d925fab4c
ZenithClown <debmalyapramanik.005@gmail.com>
2018-12-12 09:14:11 +0530

    🛠️ modernize setup.py file, with controls on pyproject.toml All settings for module setup are to be maintained through pyproject.toml file; Updated the file content to include keywords, author details, added intended audience classifiers, and project urls; Fixed setup.py file to be a minimal version calling the setup() function

--- setup.py
+++ setup.py
@@ -1,36 +1,9 @@
 #!/usr/bin/env python
 
-import os
-import sys
-
 from setuptools import setup
-from setuptools import find_packages
 
-sys.path.append(
-    os.path.join(os.path.dirname(__file__))
-)
-
-import algotraders
-
-setup(
-    name = "algotraders",
-    version = algotraders.__version__,
-    description = "Unified Algorithimic Tradng using Broker's API",
-    long_description = open("README.md", "r").read(),
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/PyUtility/algotraders",
-    packages = find_packages(
-        exclude = ["tests*", "examples*"]
-    ),
-    classifiers = [
-        "Development Status :: 1 - Planning",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
-    ],
-    python_requires = ">=3.12"
-)
+if __name__ == "__main__":
+    # all the settings are moved to pyproject.toml with
+    # minimal setup() calling from the setup.py file as recommended
+    # per the latest PyPI documentation on module development
+    setup()
